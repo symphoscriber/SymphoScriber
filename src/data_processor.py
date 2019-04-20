353f0615df6bc2193566a710524dc1e332d5c927
decaf <rarunok@yahoo.com>
2019-04-20 19:13:14 -0400

    fixed something

--- src/data_processor.py
+++ src/data_processor.py
@@ -18,13 +18,14 @@ def windowing(spec:tuple, n_window:int) -> tuple:
     """
     break the input spectrogram into separate windows to prepare training data
     """
-    spec_size = len(spec[1])
+    spec_size = len(spec[2]) # time points
     window_size = math.ceil(spec_size/n_window)
     
     for i in range(n_window):
         try:
-            window_spec = spec[0][i*window_size:(i+1)*window_size]
-            window_t = spec[1][i*window_size:(i+1)*window_size]
-            yield (window_spec, window_t)
+            window_sample = spec[0][:, i*window_size:(i+1)*window_size]
+            window_freq = spec[1]
+            window_t = spec[2][i*window_size:(i+1)*window_size]
+            yield (window_sample, window_freq, window_t)
         except GeneratorExit:
             raise Exception("Something wrong with generator")
